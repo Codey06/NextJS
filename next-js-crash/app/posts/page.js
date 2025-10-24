423d059eb4a07ecfab59ae8c453b0e53b38dd2b4
Mustaf Jirow <codey06@Mustafs-MacBook-Air.local>
2025-10-24 15:13:03 +0300

    Added new Update

--- next-js-crash/app/posts/page.js
+++ next-js-crash/app/posts/page.js
@@ -1,20 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
+import List from './_compenents /List'
 
 const PostsPage = async () => {
       const posts=await fetch(`http://localhost:3000/api/post`)
       const postsData =await posts.json()
   return (
-    <>
-{
-  postsData.map((post) =>(
-    <div key={post.id}>
-      <Link href={`/posts/${post.id}`} className="list-item">{post.body}</Link>
-    </div>
-  ))
- }
+    <div className='px-4 sm:px-6 lg:px-8 mt-10'>
+    <div className='sm:flex sm:items-center'>
+    <div className='sm:flex-auto'>
+                   
+             <h1 className='text-base font-semibold leading-6 text-gray-900'>Posts</h1>
+                    <p className='mt-2 text-sm text-gray-700'>A list of all posts from the json file</p>
+                </div>
+                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <Link
 
-    </>
+                        href={'/posts/new'}
+                        type="button"
+                        className="block rounded-md bg-rose-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
+                    >
+                        Add Posts
+                    </Link>
+                </div>
+                </div>
+<List postsData={postsData}/>
+
+    </div>
   )
 }
 
